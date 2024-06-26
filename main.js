785406af69d30705019745a5344a99522cdab8dc
kvtrice <katricemountford@gmail.com>
2024-06-26 18:01:22 +1000

    Adds Desktop Items and Highlight on click

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 import { getCurrentTime } from "./modules/taskbar.js";
 import { toggleMenu } from "./modules/menu.js";
 import { createMenuItemFolder, createMenuItem } from "./modules/menu.js";
+import { highlightOnClick } from "./modules/apps.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-	// Create Menu Items
-	createMenuItemFolder();
-	createMenuItem();
+// Create Menu Items
+createMenuItemFolder();
+createMenuItem();
 
-	// Get current time every second
-	getCurrentTime();
-	setInterval(getCurrentTime, 1000);
-});
-// DOM Elements
-const startButton = document.getElementById("start");
-const menu = document.getElementById("menu");
+// Get current time every second
+getCurrentTime();
+setInterval(getCurrentTime, 1000);
+
+// Highlight Desktop Apps on Click
+highlightOnClick();
 
 // Open  & Close Start Menu
+const startButton = document.getElementById("start");
 startButton.addEventListener("click", toggleMenu);
 
 // Click anywhere outside menu to close it
 document.addEventListener("click", event => {
+	const menu = document.getElementById("menu");
 	if (!menu.contains(event.target) && !startButton.contains(event.target)) {
 		menu.classList.remove("open");
 		startButton.classList.remove("open");
---
+++ modules/apps.js
@@ -0,0 +1,37 @@
+const desktopItems = [{
+    
+}]
+
+export const highlightOnClick = () => {
+	const apps = document.querySelectorAll(".app");
+
+	const deselectAll = () => {
+		apps.forEach(app => {
+			const appText = app.querySelector(".app__text");
+			const appImg = app.querySelector(".app__img");
+			if (appText.classList.contains("clicked--text")) {
+				appText.classList.remove("clicked--text");
+			}
+
+			if (appImg.classList.contains("clicked--img")) {
+				appImg.classList.remove("clicked--img");
+			}
+		});
+	};
+
+	apps.forEach(app => {
+		const appText = app.querySelector(".app__text");
+		const appImg = app.querySelector(".app__img");
+		app.addEventListener("click", () => {
+			deselectAll();
+			appText.classList.add("clicked--text");
+			appImg.classList.add("clicked--img");
+		});
+	});
+
+	document.addEventListener("click", event => {
+		if (!event.target.closest(".app")) {
+			deselectAll();
+		}
+	});
+};
